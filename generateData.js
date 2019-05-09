92878008a3a7a88705fce394f61a96e69a60a4da
Mariam <mariam.a.qasim@gmail.com>
2019-05-09 11:15:47 -0400

    changed json

--- generateData.js
+++ generateData.js
@@ -1,62 +1,52 @@
-var faker = require('faker');
+var faker = require("faker");
 
 var database = { hotels: [] };
 
-
 for (var i = 1; i <= 30; i++) {
-    database.hotels.push({
-        id: i,
-        name: faker.company.companyName() + ' Hotel',
-        description: faker.lorem.paragraph(),
-        distance_to_venue: faker.random.number({ 'min': 1, 'max': 400 }) + ' meters',
-        rating: faker.random.number({ 'min': 0, 'max': 5 }),
-        price_category: faker.random.arrayElement(['low', 'medium', 'high']),
-        amenities: [
-            faker.random.arrayElement([' WIFI', ' A/C', ' Breakfast Included']),
-            faker.random.arrayElement([' Free Parking', ' Swimming Pool', ' Spa']),
-            faker.random.arrayElement([' Pets Allowed', ' Gym', ' Laundry Services'])
-        ],
-        address: faker.address.streetAddress(),
-        city: faker.address.city(1),
-        country: faker.address.country(),
-        phone: faker.phone.phoneNumber(),
-        date: faker.date.between('2015-01-01', '2015-12-31', '##-##-##'),
-        image: faker.random.arrayElement(
-            [
-                '../assets/hotel1.jpg',
-                '../assets/hotel2.jpg',
-                '../assets/hotel3.jpg',
-                '../assets/hotel4.jpg',
-                '../assets/hotel5.jpg',
-                '../assets/hotel6.jpg',
-                '../assets/hotel7.jpg',
-            ]
-        ),
-        room1: {
-            id: i + 1,
-            name: faker.company.companyName() + ' Room',
-            description: faker.lorem.paragraph(),
-            max_occupancy: faker.random.number({ 'min': 1, 'max': 4 }),
-            price_in_usd: '$' + faker.random.number({ 'min': 1, 'max': 100, 'precision': 0.01 }) + '/night',
-        },
-        room2: {
-            id: i + 2,
-            name: faker.company.companyName() + ' Room',
-            description: faker.lorem.paragraph(),
-            max_occupancy: faker.random.number({ 'min': 1, 'max': 4 }),
-            price_in_usd: '$' + faker.random.number({ 'min': 1, 'max': 100, 'precision': 0.01 }) + '/night',
-        },
-        room3: {
-            id: i + 3,
-            name: faker.company.companyName() + ' Room',
-            description: faker.lorem.paragraph(),
-            max_occupancy: faker.random.number({ 'min': 1, 'max': 4 }),
-            price_in_usd: '$' + faker.random.number({ 'min': 1, 'max': 100, 'precision': 0.01 }) + '/night',
-        }
-    });
+  database.hotels.push({
+    id: i,
+    name: faker.company.companyName() + " Hotel",
+    description: faker.lorem.paragraph(),
+    distance_to_venue: faker.random.number({ min: 1, max: 400 }) + " meters",
+    rating: faker.random.number({ min: 0, max: 5 }),
+    price_category: faker.random.arrayElement(["low", "medium", "high"]),
+    amenities: [
+      faker.random.arrayElement([" WIFI", " A/C", " Breakfast Included"]),
+      faker.random.arrayElement([" Free Parking", " Swimming Pool", " Spa"]),
+      faker.random.arrayElement([" Pets Allowed", " Gym", " Laundry Services"])
+    ],
+    address: faker.address.streetAddress(),
+    image: faker.random.arrayElement([
+      "../assets/hotel1.jpg",
+      "../assets/hotel2.jpg",
+      "../assets/hotel3.jpg",
+      "../assets/hotel4.jpg",
+      "../assets/hotel5.jpg",
+      "../assets/hotel6.jpg",
+      "../assets/hotel7.jpg"
+    ]),
+    room1: {
+      id: i + 1,
+      name: faker.company.companyName() + " Room",
+      description: faker.lorem.paragraph(),
+      max_occupancy: faker.random.number({ min: 1, max: 4 }),
+      price_in_usd: faker.random.number({ min: 1, max: 100, precision: 0.01 })
+    },
+    room2: {
+      id: i + 2,
+      name: faker.company.companyName() + " Room",
+      description: faker.lorem.paragraph(),
+      max_occupancy: faker.random.number({ min: 1, max: 4 }),
+      price_in_usd: faker.random.number({ min: 1, max: 100, precision: 0.01 })
+    },
+    room3: {
+      id: i + 3,
+      name: faker.company.companyName() + " Room",
+      description: faker.lorem.paragraph(),
+      max_occupancy: faker.random.number({ min: 1, max: 4 }),
+      price_in_usd: faker.random.number({ min: 1, max: 100, precision: 0.01 })
+    }
+  });
 }
 
-
 console.log(JSON.stringify(database));
-
-
