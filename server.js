e653e9ee0ea68975d9e3434169bb2e3b12df8afd
Mariam <mariam.a.qasim@gmail.com>
2019-05-09 14:54:31 -0400

    working on adding a hotel

--- server.js
+++ server.js
@@ -1,59 +1,77 @@
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const jsonServer = require('json-server');
-const jwt = require('jsonwebtoken');
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
 
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'));
+const router = jsonServer.router("./db.json");
+const userdb = JSON.parse(fs.readFileSync("./users.json", "UTF-8"));
+const hoteldb = JSON.parse(fs.readFileSync("./db.json", "UTF-8"));
 
 server.use(jsonServer.defaults());
 
-const SECRET_KEY = '123456789'
-const expiresIn = '12h'
+const SECRET_KEY = "123456789";
+const expiresIn = "12h";
 
-function createToken(payload) {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn })
+function createHotel(data) {
+  console.log("DATA=====", data);
+  hoteldb.hotels.push(data);
 }
 
-function verifyToken(token) {
-    return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err)
-}
+// function createToken(payload) {
+//   console.log("payload?", payload);
 
-function isAuthenticated({ email, password }) {
-    return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
-}
+//   return jwt.sign(payload, SECRET_KEY, { expiresIn });
+// }
 
-server.post('/auth/login', (req, res) => {
-    const { email, password } = req.body
-    if (isAuthenticated({ email, password }) === false) {
-        const status = 401
-        const message = 'Incorrect email or password'
-        res.status(status).json({ status, message })
-        return
-    }
-    const access_token = createToken({ email, password })
-    res.status(200).json({ access_token })
-})
+// function verifyToken(token) {
+//   return jwt.verify(token, SECRET_KEY, (err, decode) =>
+//     decode !== undefined ? decode : err
+//   );
+// }
 
-server.use(/^(?!\/auth).*$/, (req, res, next) => {
-    if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-        const status = 401
-        const message = 'Bad authorization header'
-        res.status(status).json({ status, message })
-        return
-    }
-    try {
-        verifyToken(req.headers.authorization.split(' ')[1])
-        next()
-    } catch (err) {
-        const status = 401
-        const message = 'Error: access_token is not valid'
-        res.status(status).json({ status, message })
-    }
-})
+// function isAuthenticated({ email, password }) {
+//   return (
+//     userdb.users.findIndex(
+//       user => user.email === email && user.password === password
+//     ) !== -1
+//   );
+// }
 
+// server.post("/new", (req, res) => {
+//   const { email, password } = req.body;
 
-server.use(router)
+//   const access_token = createToken({ email, password });
+//   res.status(200).json({ access_token });
+// });
 
-server.use('/api', router);
+server.post("/hotels", (req, res) => {
+  console.log("POST??=========================================", req, req.body);
+  let parsed = JSON.stringify(req.body);
+  const newHotel = createhotel(parsed);
+  res.status(200).json(newHotel);
+});
+
+// server.use(/^(?!\/auth).*$/, (req, res, next) => {
+//   if (
+//     req.headers.authorization === undefined ||
+//     req.headers.authorization.split(" ")[0] !== "Bearer"
+//   ) {
+//     const status = 401;
+//     const message = "Bad authorization header";
+//     res.status(status).json({ status, message });
+//     return;
+//   }
+//   try {
+//     verifyToken(req.headers.authorization.split(" ")[1]);
+//     next();
+//   } catch (err) {
+//     const status = 401;
+//     const message = "Error: access_token is not valid";
+//     res.status(status).json({ status, message });
+//   }
+// });
+
+server.use(router);
+
+server.use("/api", router);
